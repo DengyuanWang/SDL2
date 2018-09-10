1587d249b8634c660e492965494927549999f72f
王登远 <wangdengyuan@er-10-131-27-244.wireless.umn.edu>
2018-09-10 15:57:06 -0500

    Initial Commit

---
+++ SDL2_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL2_test
+//
+//  Created by 王登远 on 2018/9/10.
+//  Copyright © 2018 王登远. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
